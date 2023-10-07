2abf363a7df4c4198c3fa8b62bf9de3efcc12d1d
toast350 <canadian.frog350@gmail.com>
2023-10-07 00:56:41 -0500

    changed the dumb switch thingy

--- main.go
+++ main.go
@@ -29,53 +29,14 @@ func main() {
 		}
 	}
 
-	var one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty int = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	counters := make([][]int, diceSides+1)
 
 	for i := 0; i < len(theSliceWithTheNumbers); i++ {
-		switch theSliceWithTheNumbers[i] {
-		case 1:
-			one += 1
-		case 2:
-			two += 1
-		case 3:
-			three += 1
-		case 4:
-			four += 1
-		case 5:
-			five += 1
-		case 6:
-			six += 1
-		case 7:
-			seven += 1
-		case 8:
-			eight += 1
-		case 9:
-			nine += 1
-		case 10:
-			ten += 1
-		case 11:
-			eleven += 1
-		case 12:
-			twelve += 1
-		case 13:
-			thirteen += 1
-		case 14:
-			fourteen += 1
-		case 15:
-			fifteen += 1
-		case 16:
-			sixteen += 1
-		case 17:
-			seventeen += 1
-		case 18:
-			eighteen += 1
-		case 19:
-			nineteen += 1
-		case 20:
-			twenty += 1
-		}
+		counters[theSliceWithTheNumbers[i]] = append(counters[theSliceWithTheNumbers[i]], theSliceWithTheNumbers[i])
 	}
+	counters = append(counters[1:])
 
-	// Divide the thingies by 4 later
-	fmt.Printf("Percentages:\n1: %v\n2: %v\n3: %v\n4: %v\n5:% v\n6: %v\n7: %v\n8: %v\n9: %v\n10: %v\n11: %v\n12: %v\n13: %v\n14: %v\n15: %v\n16: %v\n17: %v\n18: %v\n19: %v\n20: %v\n", one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty)
+	for i := 0; i < len(counters); i++ {
+		fmt.Printf("%v: %v\n", i+1, len(counters[i]))
+	}
 }
